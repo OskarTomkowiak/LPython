d71766c44f1bbf699e9e37d5835d7126f28c7621
OskarTomkowiak <104001410+OskarTomkowiak@users.noreply.github.com>
2022-06-19 18:32:30 +0200

    Add files via upload

---
+++ Ackermann_function.py
@@ -0,0 +1,11 @@
+em = input('Enter m: '); m = int(em)
+en = input('Enter n: '); n = int(en)
+def A(m,n):
+    if m == 0:
+        return n + 1
+    elif m > 0 and n == 0:
+        return A(m-1,1)
+    elif m > 0 and n > 0:
+        return A(m-1,A(m,n-1))
+result = A(m,n)
+print(result)
