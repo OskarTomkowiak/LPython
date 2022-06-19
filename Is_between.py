905c3e72e67be8e750df92b4893eace2a31c6937
OskarTomkowiak <104001410+OskarTomkowiak@users.noreply.github.com>
2022-06-19 17:37:22 +0200

    Add files via upload

---
+++ Is_between.py
@@ -0,0 +1,10 @@
+def is_between(x,y,z):
+    if x <= y <= z :
+        print('True')
+    else:
+        print('False')
+print("Is between program please enter x,y,z")
+ax = input("enter x: ")
+ay = input("enter y: ")
+az = input("enter z: ")
+is_between(ax,ay,az)
